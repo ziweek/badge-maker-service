090326911cbbfafd3a83f340858925c6fa426ef5
Austin <austin.jiuk.kim@gmail.com>
2022-10-27 05:21:31 +0900

    Add Header

--- src/badge/badge.controller.ts
+++ src/badge/badge.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get, Query, Render } from '@nestjs/common';
+import { Controller, Get, Header, Query, Render } from '@nestjs/common';
 import { BadgeService } from './badge.service';
 
 @Controller('badge')
 export class BadgeController {
     constructor(private badgeService: BadgeService){};
 
+    @Header("Content-Type", "image/svg+xml")
     @Get()
     async getBadge(@Query('logo') logo:string,  @Query('color') color:string, @Query('name') name:string): Promise<any> {
         return this.badgeService.createBadge(logo, color, name)
